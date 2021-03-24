3eac934fbff44732dafdc765f06a9fd90b3b1ef2
Rafael J Garcia <ekoe299@gmail.com>
2021-03-23 18:37:06 -0700

    Installed Express and set up routes for products

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import colors from 'colors';
-import connectDB from './config/db.js';
-import products from './data/products.js'
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 
-dotenv.config();
+import productRoutes from './routes/productRoutes.js'
 
-connectDB();
+dotenv.config()
 
-const app = express();
+connectDB()
+
+const app = express()
 
 app.get('/', (req, res) => {
   res.send('API is running....')
 })
+
+app.use('/api/products', productRoutes)
 
 app.get('/api/products', (req, res) => {
   res.json(products)
