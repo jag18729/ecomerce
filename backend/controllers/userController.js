6b83ec3effcb56293d09c5aabfbe13678a5bfbf7
Rafael J Garcia <ekoe299@gmail.com>
2021-04-05 12:36:39 -0700

    Added hashed password middleware in User Model

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,9 +2,9 @@ import asyncHandler from 'express-async-handler'
 import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
-//@desc Auth the user & get token
-//@route POST /api/users/login
-//@access Public 
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
 
@@ -24,24 +24,56 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-//@desc Get user profile
-//@route GET /api/users/profile
-//@access Private
-const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('Success')
-  // const user = await User.findById(req.user._id)
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
 
-  // if (user) {
-  //   res.json({
-  //     _id: user._id,
-  //     name: user.name,
-  //     email: user.email,
-  //     isAdmin: user.isAdmin,
-  //   })
-  // } else {
-  //   res.status(404)
-  //   throw new Error('User not found')
-  // }
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
-export { authUser, getUserProfile }
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile, registerUser }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from 'express'
 const router = express.Router()
 import {
   authUser,
-  // registerUser,
+  registerUser,
   getUserProfile,
   // updateUserProfile,
   // getUsers,
@@ -12,7 +12,8 @@ import {
 } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
-// router.route('/').post(registerUser).get(protect, admin, getUsers)
+router.route('/').post(registerUser)
+  .get(protect)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 // router
