c6ffac180871c5dfa6da0f79f432f4958bea9dfe
Rafael J Garcia <ekoe299@gmail.com>
2021-04-24 10:56:03 -0700

    Fixed state reset upon loggin out, bug fixed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
       <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand><i class="far fa-plus-square" >  ProShop</i></Navbar.Brand>
+            <span id='proshopHead'><i class="far fa-plus-square" >  </i>  ProShop</span>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,6 +1,6 @@
 import {
   USER_DETAILS_REQUEST,
-  // USER_DETAILS_RESET,
+  USER_DETAILS_RESET,
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
   // USER_LIST_REQUEST,
@@ -65,8 +65,8 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
       return { loading: false, user: action.payload }
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload }
-    // case USER_DETAILS_RESET:
-    //   return { user: {} }
+    case USER_DETAILS_RESET:
+      return { user: {} }
     default:
       return state
   }
